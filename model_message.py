5b5719274a492d08c06ba298b899e7b69cf2fa13
Bander Alshammari <bander@MacBook-Pro-Bander.local>
2023-03-01 21:02:23 +0300

    refactor

--- model_message.py
+++ model_message.py
@@ -1,5 +1,6 @@
 import logging
-
+from model_client import Client
+from db import findClient
 '''
 A message sent by a process (client/server) to another. It is a null terminated string with the
 maximum length of 255 bytes (256th byte is null). First 8 bytes are the name of destination, next
@@ -12,22 +13,13 @@ the number of concurrent clients or use some other way to send the list in more 
 iterations.
 '''
 class Message:
-    def __init__(self, src, dest, content):
-        if len(src) > 8:
-            logging.error("Invalid source")
-            raise ValueError
-
-        if len(dest) > 8:
-            logging.error("Invalid destination")
-            raise ValueError
-
+    def __init__(self, src: Client, dest: Client, content: str):
+        # checking id of client don on Client object
         if len(content) > 239:
             logging.error("Invalid content")
             raise ValueError
-
-        # Add ' ' padding until its length eqauls 8 bytes
-        self.src = src.ljust(8)
-        self.dest = dest.ljust(8)
+        self.src = src
+        self.dest = dest
         self.content = content
 
     '''
@@ -36,10 +28,9 @@ Return raw message
         # next 8 bytes are the name of the source
         # content
         # null termination
-
     '''
     def serialize(self):
-        message = "".join([self.dest,self.src,self.content,'\0'])
+        message = "".join([self.dest.id,self.src.id,self.content,'\0'])
         return message
   
     '''
@@ -50,8 +41,10 @@ Take raw message and return Message instance
         if len(message) > 256 or len(message) < 17:
             logging.error("Invalid message")
             raise ValueError
-
-        destination = message[0:8]
-        source      = message[8:16]
+        
+        # destination = Client(message[0:8])
+        # source      = Client(message[8:16])
+        destination = findClient(message[0:8])
+        source = findClient(message[8:16])
         content      = message[16:-1]
         return Message(source,destination,content)
